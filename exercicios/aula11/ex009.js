992eacdd2bbf1c8c594e3942a650c9480e80a2a3
Falcaomil <107655148+Falcaomil@users.noreply.github.com>
2022-07-11 21:18:01 -0300

    Initial commit

---
+++ exercicios/aula11/ex009.js
@@ -0,0 +1,10 @@
+// \n quebra de linha
+
+var pais = 'brasil'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro \n')
+}
+else{
+    console.log('Você é Estangeiro \n')
+}
